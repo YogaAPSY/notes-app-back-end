c5129e248dc1a1b6ad8ef56457d831ac7b224115
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2021-06-14 21:33:30 +0700

    second

--- src/server.js
+++ src/server.js
@@ -1,16 +1,32 @@
-const notesPlugin = require('./notesPlugin');
-const Hapi = require('@hapi/hapi')
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+ 
  
 const init = async () => {
-  const server = Hapi.server();
-  
-  // registrasi satu plugin
+  const notesService = new NotesService();
+ 
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  server.route(routes);
+ 
   await server.register({
-    plugin: notesPlugin,
-    options: { notes: [] },
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
   });
  
   await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
  
 init();
