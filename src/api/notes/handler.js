1c89bbe5f5d15d7ac0efa7a148e59ef61ab499ac
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2021-06-15 14:35:39 +0700

    restructure with hapi plugin

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,12 @@
 class NotesHandler {
   constructor(service) {
     this._service = service;
+
+    this.postNoteHandler = this.postNoteHandler.bind(this);
+    this.getNotesHandler = this.getNotesHandler.bind(this);
+    this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
+    this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
+    this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
   postNoteHandler(request, h) {
