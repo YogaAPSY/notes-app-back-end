d4e02ef1a03b5f3a5cf857bebd791f34829bbb74
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2021-06-17 07:21:00 +0700

    sss

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
-const NotesValidator = require('./validator/notes');
  
 const init = async () => {
   const notesService = new NotesService();
- 
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -16,13 +14,10 @@ const init = async () => {
     },
   });
  
-  server.route(routes);
- 
   await server.register({
     plugin: notes,
     options: {
       service: notesService,
-      validator: NotesValidator,
     },
   });
  
